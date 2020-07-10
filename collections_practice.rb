80125ed6dcf712ded8981ec6a1d2e8fc55fd7357
Amanda Walker <walker.akw@gmail.com>
2020-07-10 05:40:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,5 +39,5 @@ def sum_array(addition)
 end
 
 def add_s(plural)
-  plural.each {|index| "index[0]" << "s" + index[1] + index[2..-1] << "s"}
+  plural.each_with_index {|element, index| if index != 1 "#{element}s"}
 end
