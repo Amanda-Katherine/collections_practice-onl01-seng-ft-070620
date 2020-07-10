a8fedbfaec21ffe762f6b892e25515bc2762300d
Amanda Walker <walker.akw@gmail.com>
2020-07-10 04:33:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,14 @@
 def sort_array_asc(increase)
   increase.sort
 end
+
+def sort_array_desc(decrease)
+  decrease.sort.reverse
+end
+
+def sort_array_char_count(word)
+  word.length.sort
+end
+
+
+  
